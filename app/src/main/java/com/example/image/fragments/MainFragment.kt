bf6c20c0a4c46492b1b597b2cf970b2595471224
kishore-pt6813 <kishore-pt6813@kishore-pt6813.csez.zohocorpin.com>
2023-01-18 11:59:05 +0530

    first commit

---
+++ app/src/main/java/com/example/image/fragments/MainFragment.kt
@@ -0,0 +1,61 @@
+package com.example.image.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.content.ContextCompat
+import androidx.core.view.drawToBitmap
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import coil.load
+import com.example.image.R
+import com.example.image.databinding.FragmentMainBinding
+import com.example.image.models.ImageViewModel
+import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.lang.Thread.sleep
+
+
+class MainFragment : Fragment() {
+
+    lateinit var binding:FragmentMainBinding
+    private val viewModel:ImageViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =  FragmentMainBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.goButton.setOnClickListener{
+            var url =  binding.textInput.text.toString()
+            if(url!=""){
+                viewModel.setImageUrl(url)
+                binding.image.visibility = View.VISIBLE
+                binding.downloadButton.visibility = View.VISIBLE
+                var result=binding.image.load(viewModel.getImageUrl()){
+                    placeholder(R.drawable.loading_img)
+                    error(R.drawable.ic_connection_error)
+                }
+
+            }
+        }
+
+        binding.downloadButton.setOnClickListener{
+            lifecycleScope.launch(Dispatchers.IO) {
+                viewModel.downloadImage(requireContext(), viewModel.getImageUrl(),"image.jpg")
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/image/models/ImageViewModel.kt
@@ -0,0 +1,142 @@
+package com.example.image.models
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.image.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.*
+
+// Name of Notification Channel for verbose notifications of background work
+@JvmField val VERBOSE_NOTIFICATION_CHANNEL_NAME: CharSequence =
+    "Notifications"
+const val VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION =
+    "Shows notification"
+@JvmField val NOTIFICATION_TITLE: CharSequence = "Notification"
+const val CHANNEL_ID = "ID_NOTIFICATION"
+const val NOTIFICATION_ID = 1
+
+class ImageViewModel:ViewModel() {
+    private var _imageUrl= MutableLiveData<String>("")
+    val imageUrl:LiveData<String> = _imageUrl
+
+
+
+    //Setter
+    fun setImageUrl(url:String){
+        _imageUrl.value = url
+    }
+
+    //Getter
+    fun getImageUrl():String{
+        return _imageUrl.value!!
+    }
+
+    fun makeStatusNotification(message: String, context: Context) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = VERBOSE_NOTIFICATION_CHANNEL_NAME
+            val description = VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+
+            // Add the channel
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+            notificationManager?.createNotificationChannel(channel)
+        }
+
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(NOTIFICATION_TITLE)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVibrate(LongArray(0))
+
+        // Show the notification
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
+    }
+
+    suspend fun downloadImageFromURL(context: Context, imageURL: String): Uri? {
+        return withContext(Dispatchers.IO) {
+            val url = URL(imageURL)
+            val inputStream = url.openStream()
+            val values = ContentValues().apply {
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            }
+            val uri = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            if (uri != null) {
+                try {
+                    val outputStream = context.contentResolver.openOutputStream(uri)
+                    if (outputStream != null) {
+                        inputStream.use { input ->
+                            outputStream.use { output ->
+                                input.copyTo(output)
+                            }
+                        }
+                    }
+                } catch (e: IOException) {
+                    context.contentResolver.delete(uri, null, null)
+                    e.printStackTrace()
+                }
+            }
+            uri
+        }
+
+    }
+
+
+    suspend fun downloadImage(context: Context, url: String, fileName: String) {
+        try {
+            val url = URL(url)
+            val inputStream = url.openStream()
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+            }
+
+            val uri = withContext(Dispatchers.IO) {
+                context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            }
+
+            val outputStream = context.contentResolver.openOutputStream(uri!!)
+            inputStream.use { it.copyTo(outputStream!!) }
+            outputStream?.close()
+            makeStatusNotification("Download completed",context)
+        } catch (e: MalformedURLException) {
+            makeStatusNotification("Invalid Url",context)
+        } catch (e: Exception) {
+            makeStatusNotification("Couldn't download",context)
+        }
+    }
+
+}
